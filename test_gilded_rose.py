2106161f2570488a12dbc5072fcaf7ec7bceca0b
jose4663 <jose_4663@Hotmail.com>
2022-02-18 21:30:24 -0800

    TC added: sell in, quality, passed sell, Aged Brie, Max Quality, Sulfuras (FAIL), Backstage pass (FAIL) Need comments

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,49 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("foo", items[0].name)
 
+    def test_sellin(self):
+        items = [Item("foo", 11, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(10, items[0].sell_in)
+
+    def test_quality(self):
+        items = [Item("foo", 11, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(0, items[0].quality)
+
+    def test_sellpassed(self):
+        items = [Item("foo", 0, 2)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(0, items[0].quality)
+
+    def test_agedbrie(self):
+        items = [Item("Aged Brie", 11, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(1, items[0].quality)
+
+    def test_maxquality(self):
+        items = [Item("Aged Brie", 11, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(50, items[0].quality)
+
+    def test_sulfuras(self):
+        items = [Item("Sulfuras", 11, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(11, items[0].sell_in)
+        self.assertEquals(50, items[0].quality)
+
+    def test_backstagespasses(self):
+        items = [Item("Backstage passes", 7, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(6, items[0].sell_in)
+        self.assertEquals(12, items[0].quality)
         
 if __name__ == '__main__':
     unittest.main()
