3e5c8725c46d998c976e93d2b9de60c6529bc5fb
jose4663 <jose_4663@Hotmail.com>
2022-02-24 19:33:41 -0800

    Check

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,8 +11,7 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual("foo", items[0].name)
 
-
-    def test_sellin(self):
+    def test_sell_in(self):
         items = [Item("foo", 11, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
@@ -70,6 +69,7 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(1, items[0].sell_in)
         self.assertEqual(0, items[0].quality)
-        
+
+
 if __name__ == '__main__':
     unittest.main()
