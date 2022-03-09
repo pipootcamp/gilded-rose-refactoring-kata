02048a43a6508f0cceb1b16ff40b25f48c9f6fc2
fmarquez24 <97994064+fmarquez24@users.noreply.github.com>
2022-03-08 22:50:22 -0500

    8/03/22

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,41 +7,68 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            self.update_one_item(item)
 
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        if "Conjured" not in item.name:
-                            item.quality = item.quality - 1
-                        else:
-                            item.quality = item.quality - 2
-                    else:
-                        item.quality = 80
+    def update_one_item(self, item):
+        self.update_item_quality(item)
+        self.update_sell_in(item)
+        if self.has_expired(item):
+            self.update_expired(item)
 
+    def has_expired(self, item):
+        return item.sell_in < 0
+
+    def update_expired(self, item):
+        if self.is_cheese(item):
+            if item.quality < 50:
+                item.quality = item.quality + 1
+        else:
+            if self.is_concert_ticket(item):
+                item.quality = item.quality - item.quality
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                if item.quality > 0:
+                    if self.is_sulfuras(item):
+                        return
+                    item.quality = item.quality - 1
 
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+    def update_sell_in(self, item):
+        if self.is_sulfuras(item):
+            return
+        item.sell_in = item.sell_in - 1
+
+    def update_item_quality(self, item):
+        if self.is_cheese(item) or self.is_concert_ticket(item):
+            self.update_cheese_and_ticket_quality(item)
+
+        else:
+            if item.quality > 0:
+                if self.is_sulfuras(item):
+                    item.quality = 80
                 else:
+                    if "Conjured" in item.name:
+                        item.quality = item.quality - 2
+                    else:
+                        item.quality = item.quality - 1
+
+    def update_cheese_and_ticket_quality(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if self.is_concert_ticket(item):
+                if item.sell_in < 11:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+
+    def is_concert_ticket(self, item):
+        return item.name == "Backstage passes to a TAFKAL80ETC concert"
+
+    def is_cheese(self, item):
+        return item.name == "Aged Brie"
+
+    def is_sulfuras(self, item):
+        return item.name == "Sulfuras, Hand of Ragnaros"
 
 
 class Item:
