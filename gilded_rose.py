b9ce5d6fd17140e99391aaf8a632806943601217
fmarquez24 <97994064+fmarquez24@users.noreply.github.com>
2022-03-21 22:05:41 -0400

    3/21/22 test Safety_commit
    regresar aqui si falla pipo

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-from ItemCategory import ItemCategory, decrease_quality, has_expired, is_concert_ticket, is_cheese, is_sulfuras
+from ItemCategory import ItemCategory
 
 
 class GildedRose(object):
@@ -10,46 +9,66 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             item_category = ItemCategory(item)
-            self.update_one_item(item, item_category)
+            self.update_one_item(item)
 
-    def update_one_item(self, item, category):
-        self.update_item_quality(item, category)
-        self.update_sell_in(item, category)
-        if has_expired(self.sell_in, item):
-            self.update_expired(item, category)
+    def update_one_item(self, item):
+        self.update_item_quality(item)
+        self.update_sell_in(item)
+        if self.has_expired(item):
+            self.update_expired(item)
 
-    def update_expired(self, item, category):
-        if is_cheese(self.name, item):
-            self.increase_quality(item, category)
-        elif is_concert_ticket(self.name, item):
+    def update_expired(self, item):
+        if self.is_cheese(item):
+            self.increase_quality(item)
+        elif self.is_concert_ticket(item):
             item.quality = 0
-        elif is_sulfuras(self.name, item):
+        elif self.is_sulfuras(item):
             return
         else:
-            decrease_quality(item, category, 1)
+            self.decrease_quality(item, 1)
 
-    def update_sell_in(self, item, category):
-        if is_sulfuras(self.name, item):
+    def update_sell_in(self, item):
+        if self.is_sulfuras(item):
             return
         item.sell_in = item.sell_in - 1
 
-    def update_item_quality(self, item, category):
-        if is_cheese(self.name, item) or is_concert_ticket(self.name, item):
+    def update_item_quality(self, item):
+        if self.is_cheese(item) or self.is_concert_ticket(item):
             self.update_cheese_and_ticket_quality(item)
-        elif is_sulfuras(self.name, item):
+        elif self.is_sulfuras(item):
             item.quality = 80
         elif "Conjured" in item.name:
-            decrease_quality(item, 2, 1)
+            self.decrease_quality(item, 2)
         else:
-            decrease_quality(item, 1, 1)
+            self.decrease_quality(item, 1)
 
     def update_cheese_and_ticket_quality(self, item):
         self.increase_quality(item)
-        if is_concert_ticket(self.name, item):
+        if self.is_concert_ticket(item):
             if item.sell_in < 11:
                 self.increase_quality(item)
             if item.sell_in < 6:
                 self.increase_quality(item)
+
+    def increase_quality(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+
+    def decrease_quality(self, item, i):
+        if item.quality > 0:
+            item.quality = item.quality - i
+
+    def has_expired(self, item):
+        return item.sell_in < 0
+
+    def is_concert_ticket(self, item):
+        return item.name == "Backstage passes to a TAFKAL80ETC concert"
+
+    def is_cheese(self, item):
+        return item.name == "Aged Brie"
+
+    def is_sulfuras(self, item):
+        return item.name == "Sulfuras, Hand of Ragnaros"
 
 
 class Item:
@@ -60,3 +79,5 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
